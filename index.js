07b2aa40234a239b10edff42eee97ca3ddbf9ee0
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2025-03-13 13:19:05 +0100

    testy2

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
 const express = require("express");
-const app = express();
-// const axios = require("axios");
+const axios = require('axios').default;
+const cors = require("cors");
+const morgan = require("morgan");
+const fetch = require('node-fetch');
 
-// const daneLogin = {
-// 	username: "kf63083@amu.edu.pl",
-// 	password: "Krychu11L19*63083",
-// };
+
+require("dotenv").config();
+
+
+const app = express();
+
+app.use(morgan("angle"));
+app.use(cors());
+
+// app.get("/login", async(req, res) => {
+// 	console.log(req.body)
+// 	function fetchUsers() {
+// 			fetch('https://amunatcoll.pl:8000/login/',
+// 			  {
+// 				mode: 'cors',
+// 				method: 'POST',
+// 				headers: {
+// 				  'Accept': 'application/json',
+// 				  'Content-Type': 'application/json',
+// 				  'access-control-allow-credentials': 'true',
+// 				}, body: JSON.stringify({
+// 				  "username": "kf63083@amu.edu.pl",
+// 				  "password": "Krychu11L19*63083"
+// 				})
+// 			  })
+// 			  .then((res) => res.json())
+// 			  .then((dane) => res.json(dane))
+// 		  };
+// 		  fetchUsers();
+
+// 	}) 
+
+	app.get("/login", async(req, res) => {
+		(async () => {
+			try {
+				await fetch("https://pixabay.com/api/videos/?key=26584808-de6453e81ed644e8a5f6a08c0&q=yellow+flowers")
+				.then((response) => response.json())
+				.then((json) => {
+					res.json(json.items)
+				})
+			} catch (error) {
+				console.log(error.response.body)
+			}
+		})()
+		}) 
+
+
+
+
+
+
+
+
+
+
+
 
 // const tokenIS = [];
 
@@ -26,5 +80,5 @@ app.get("/login2", (req, res) => {
 });
 
 app.listen(8888, () => {
-	console.log("aplikacja działa!!!!");
+	console.log("aplikacja działa!!!!2");
 });
